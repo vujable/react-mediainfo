a748333e3bec26ed9da55a1637119b28cd4632fe
Vukasin <vukasin.pesic@footballerista.com>
2020-12-05 15:07:41 +0100

    'update'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import MediaInfo from 'mediainfo.js';
 
-export function test() {
-    console.warn('testing package');
-}
 export function getMediaInfo() {
-    console.warn('starting getMediaInfo');
     return new Promise((resolve) => {
         MediaInfo().then((mediainfo) => {
             resolve(mediainfo);

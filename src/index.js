7e31e02bdd06af086b65c91032d85772a7a69454
Vukasin <vukasin.pesic@footballerista.com>
2020-12-05 16:03:33 +0100

    'update'

--- src/index.js
+++ src/index.js
@@ -4,22 +4,3 @@ import MediaInfo from 'mediainfo.js';
 export function getMediaInfo() {
     MediaInfo();
 }
-
-export class App extends Component {
-    constructor(props){
-        super(props)
-        this.mediaInfo = getMediaInfo();
-    }
-    render(){
-        return(
-            <div>
-                <button onClick={() =>{
-                    console.log(this.mediaInfo);
-                }}>
-                    Click
-                </button>
-            </div>
-        )
-    }
-}
-

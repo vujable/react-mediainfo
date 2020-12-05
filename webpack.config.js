0b495a6ca2dba472e18c38c7e5cfa573fcd8138f
Vukasin <vukasin.pesic@footballerista.com>
2020-12-05 13:13:05 +0100

    'update'

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,8 @@
 const path = require('path');
 
-const CopyPlugin = require('copy-webpack-plugin');
-
-const wasmFile = path.resolve(__dirname, 'node_modules', 'mediainfo.js', 'dist', 'MediaInfoModule.wasm');
-
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: wasmFile, to: '.' }]
-        })
-    ],
     output: {
         path: path.resolve('lib'),
         filename: 'react-mediainfo.js',

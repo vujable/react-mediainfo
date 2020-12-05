9eeba070d6cbc8253fc80697215bd32189795b4f
Vukasin <vukasin.pesic@footballerista.com>
2020-12-05 14:32:39 +0100

    'update'

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-import React , {Component} from 'react'
-import MediaInfo from 'mediainfo.js';
+//import React , {Component} from 'react'
+//import MediaInfo from 'mediainfo.js';
 
 export const test = () => {
     return "test success"
 }
-export const getMediaInfo = () => {
-    return new Promise((resolve) => {
-        MediaInfo().then((mediainfo) => {
-            resolve(mediainfo);
-        });
-    });
-};
-
-class Index extends Component {
-    
-}
-export default Index;
--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,7 @@ module.exports = {
         path: path.resolve('lib'),
         filename: 'react-mediainfo.js',
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    },
     node: {
         fs: 'empty'
-    },
-    resolve: {
-        extensions: ['*', '.js']
     }
 };

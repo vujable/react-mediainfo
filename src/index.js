00293ea571fbf30e33d0f677259840b584835b44
Vukasin <vukasin.pesic@footballerista.com>
2020-12-05 14:20:59 +0100

    'update'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import React , {Component} from 'react'
 import MediaInfo from 'mediainfo.js';
 
+export const test = () => {
+    return "test success"
+}
 export const getMediaInfo = () => {
     return new Promise((resolve) => {
         MediaInfo().then((mediainfo) => {
@@ -7,3 +11,8 @@ export const getMediaInfo = () => {
         });
     });
 };
+
+class Index extends Component {
+    
+}
+export default Index;
